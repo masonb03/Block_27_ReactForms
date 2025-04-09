b41a3275a83bf33c5cbcab7dba709dfcf4c561f7
Mason <bevinsm355@gmail.com>
2025-04-09 16:36:45 -0400

    functioning submit form

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,7 +1,32 @@
-function Authenticate() {
+import {useState} from 'react'
+
+function Authenticate({token}) {
+    const [successMessage, setSuccessMessage] = useState(null)
+    const [error, setError] = useState(null)
+    
+async function handleClick(){
+    try{
+        const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate',
+            {
+                method: "GET",
+                headers: {
+                    "Content-type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            const result = await response.json()
+            setSuccessMessage(result.message)
+        }catch(error){
+            setError(error.message)
+        }
+    }
+
     return(
         <>
         <h2>Authenticate</h2>
+        {successMessage && <p>{successMessage}</p>}
+        {error && <p>{error}</p>}
+        <button onClick={handleClick}>Authenticate Token</button>
         </>
     ) 
   }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,11 +1,6 @@
 import {useState} from 'react'
 
-
-
-
-
-
-function SignUpForm() {
+function SignUpForm({setToken}) {
 const [username, setUsername] = useState("")
 const [password, setPassword] = useState("")
 const [error, setError] = useState(null)
@@ -13,29 +8,43 @@ const [error, setError] = useState(null)
 async function handleSubmit(event){
     event.preventDefault();
     try{
-        const res = await fetch('')
+        const res = await fetch('https://fsa-jwt-practice.herokuapp.com/signup',
+            {
+            method: "POST",
+            header:{
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+        const data = await res.json()
+        console.log(data)
+        setToken(res.token)
     }catch(error){
     console.log(error)
 }
-    console.log("Hello")
+   
 }
     return(
     <>
     <h2>Sign Up!</h2>
+    {
+        error && <p>{error}</p>
+    }
     <form onSubmit={handleSubmit}>
         <label>
         Username: 
         <input name="username" 
         onChange={(event) =>
-        setUsername(event.target.value)}
-        value={username}/>
+        setUsername(event.target.value)}/>
         </label>
         <label>
         Password: 
         <input name="password" 
         onChange={(event) =>
-        setPassword(event.target.value)}
-        value={password}/>
+        setPassword(event.target.value)}/>
         </label>
         <button>Submit</button>
     </form>
